5f843bb701a91a9516d5df88337d8abb87c195ba
spine26 <46239988+spine26@users.noreply.github.com>
2018-12-29 11:29:02 +0100

    Create Unban.js

---
+++ Unban.js
@@ -0,0 +1,60 @@
+// == UserScript ==
+// @name Contournement du compte ban
+// @namespace http://tampermonkey.net/
+// @ version 1.0
+// @description essaie d'aider ppl à partir du gayness de nosx!
+// @author Okumura
+// @match http://alis.io/
+// @grant GM_getResourceText
+// @grant GM_addStyle
+// @grant GM_xmlhttpRequest
+// @grant GM_getResourceURL
+// @grant GM_xmlhttpRequest
+// == / UserScript ==
+
+unsafeWindow . getJWT = function ( userid , accessesoken ) {
+    jwt =  null ; // débusque notre jwt.
+    var url =  ' / api / auth / facebook ' ;
+    méthode var =  ' POST ' ;
+    var data = {
+        ID utilisateur : ID utilisateur,
+        accèsToken : accesstoken,
+    };
+    var  succès  =  fonction ( réponse ) {
+        si ( ! réponse . jeton ) {
+            console . log ( ' webservice n'a pas pu fournir JWT ' );
+            if ( réponse . message ) {
+              $ ( ' #coingrid ' ). css ( ' display ' , ' none ' );
+                Swal ( " Impossible de se connecter via facebook: " , la réponse . un message , " erreur " );
+                $ ( " #socialcard .uk-card " ). html ( ' <p> '  +  réponse . message  +  ' </ p> ' );
+            }
+        } else {
+            jwt =  réponse . jeton ;
+            var jwt2 =  réponse . jeton . scinder ( / [ . ] / );
+            console . log ( " % ctoken: " , " font-weight: bold; " );
+            console . log ( " % c "  + jwt2 [ 0 ] +  " .% c "  + jwt2 [ 1 ] +  " .% c "  + jwt2 [ 2 ], " taille de police: 10px; color: # fb015b; " , " font -size: 10px; color: # d63aff; " , " taille de police: 10px; color: # 00b9f1; " , ' ' );
+
+            // $ ("# jwt"). text (jwt);
+            // Pourquoi cela a-t-il changé?
+            $ ( " #jwt " ). val (jwt);
+            // magasin
+            stockage local . setItem ( " jwt " , jwt);
+            // Récupérer
+            var décodé =  jwt_decode (jwt);
+            // console.log (décodé);
+            userid =  décodé . sous ;
+            // $ ('# coinswidget .modal-body'). append ('<iframe src = "https://api.paymentwall.com/api/?key=5d6242e544963c739e2c1d0288a15279&uid=' + userid + '& widget = w6_1 Largeur "" height = "" style = "width: 100%; height: 100%; min-width: 800px; min-height: 640px;" frameborder = "0"> </ iframe> ');
+            getupgrades (userid);
+            updatePlayerDetails ();
+            // essaie de renouveler les utilisateurs JWT toutes les 20 minutes
+            setInterval (renewJWT, 20  *  60  *  1000 );
+        }
+    };
+     échec  var =  fonction ( erreur ) {
+        console . log ( ' échec de la connexion avec l'erreur: ' );
+        var message =  JSON . analyse ( error . responseText );
+        console . log (message);
+    };
+    // lance l'appel api spécifié et attend sa réponse
+    apicall (url, méthode, données, succès, échec);
+};
